ce728f6e480d512f3ed141180b1151269b4d0d6d
Harley Trung <harley@socialsci.com>
2012-03-24 13:36:28 -0400

    fix 'Could not find generator auditable:migration' bug #3

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -6,5 +6,5 @@ ActiveRecord::Base.establish_connection({
 CreateTestSchema.migrate(:up)
 
 # run gem's required migration
-require(File.expand_path("../../../generators/auditable/templates/migration.rb", __FILE__))
+require(File.expand_path("../../../lib/generators/auditable/templates/migration.rb", __FILE__))
 CreateAudits.migrate(:up)
